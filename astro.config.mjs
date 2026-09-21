8742bd7914f9cd13ba91214fa3ee2110c4290c67
Mr-Aleximandro <117jald@gmail.com>
2026-09-21 00:56:24 -0600

    build ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // Reemplaza 'tu-usuario' y 'tu-repositorio' con tus datos de GitHub
   site: 'https://mr-aleximandro.github.io',
-  base: '/portafolio', // Si tu repo se llama igual que tu usuario (tu-usuario.github.io), deja base en '/'
-  integrations: [react(), tailwind()],
+  base: '/portafolio',
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
